b00b81993fc881498714281cc6f59d22695efaa4
Ben Kugler <ben@bkugler.com>
2015-06-25 17:01:25 -0400

    First stage of GUI design

--- Homunculus/Generator.hs
+++ Homunculus/Generator.hs
@@ -2,8 +2,15 @@ module Homunculus.Generator where
 
 import Homunculus.Parser
 
+import Data.List (isInfixOf)
 import Graphics.UI.Gtk hiding (Table)
+import System.Directory
+import System.FilePath ((</>))
 import System.Random
+
+{-
+  The basic (functional) code
+-}
 
 data Table = Table { name, description :: String      --These will be displayed to the user
                    , rows :: [(Int,String)]           --The meat & potatoes
@@ -18,18 +25,17 @@ generate t g = case parse (script g') $ list !! (i `mod` length list) of
   where list = concatMap (uncurry replicate) $ rows t
         (i,g') = random g :: (Int,StdGen)
 
-generateFile :: FilePath -> IO String
-generateFile fp = do
-  file <- readFile fp 
-  g <- getStdGen
-  return $ f (readTable file) g
-  where f Nothing _   = "Error: Failed to read "++fp
-        f (Just t) g  = generate t g
-
 readTable :: String -> Maybe Table
 readTable str = case reads str of
   [(x,"")]  -> Just x
   _         -> Nothing
+
+--Function for taking output of readTable and forming [Table]
+maybeToList :: [Maybe a] -> [a]
+maybeToList [] = []
+maybeToList (x:xs) = case x of
+  Nothing -> maybeToList xs
+  Just a  -> a : maybeToList xs
 
 testGen :: Table
 testGen = Table { name = "Test"
@@ -43,3 +49,82 @@ testGen = Table { name = "Test"
                          ]
                 , options = [("Option 1",["A","B","C"])]
                 }
+
+{-
+  IO code (mostly graphics-related)
+-}
+
+generateFile :: FilePath -> IO String
+generateFile fp = do
+  file <- readFile fp 
+  g <- getStdGen
+  return $ f (readTable file) g
+  where f Nothing _   = "Error: Failed to read "++fp
+        f (Just t) g  = generate t g
+
+--Clears out the box and creates the Generator UI within it
+makeGenerator :: FilePath -> VBox -> IO ()
+makeGenerator dataPath box = do
+  --Have to clear the box before doing anything else
+  mapM_ widgetDestroy =<< containerGetChildren box
+
+  {-
+    INITIALIZATION
+  -}
+  top <- hBoxNew False 0
+  bottom <- hBoxNew False 0
+  left <- vBoxNew False 0
+  right <- vBoxNew False 0
+
+  list <- listStoreNew []
+  combo <- comboBoxNewWithModel list
+  ok <- buttonNewWithLabel "Generate"
+  edit <- buttonNewWithLabel "Edit"
+  l <- labelNew $ Just "Choose A Generator:  "
+
+  {-
+    CONSTRUCTION
+  -}
+  set top     [ containerChild := l, boxChildPacking l := PackNatural
+              , containerChild := combo
+              , containerChild := ok, boxChildPacking ok := PackNatural
+              , containerChild := edit, boxChildPacking edit := PackNatural
+              ]
+  set bottom  [ containerChild := left
+              , containerChild := right
+              ]
+  set box     [ containerChild := top, boxChildPacking top := PackNatural
+              , containerChild := bottom
+              ]
+
+  {-
+    LOGIC
+  -}
+  --Load all generators from dataPath
+  fileList <- getDirectoryContents $ dataPath </> "generators"
+  files <- mapM readFile $ 
+    map ((</>) (dataPath </> "generators")) $ filter (isInfixOf ".gen") fileList
+  let tables = maybeToList $ map readTable files
+  
+  --Put all loaded generators into a ListStore, which is used by combo
+  mapM_ (listStoreAppend list) tables
+
+  --This'll cause an error if list is empty.
+  --comboBoxSetActive combo 0
+
+  --When the combo box is opened, the ListStore displays the names of the generators
+  ren <- cellRendererTextNew
+  cellLayoutPackEnd combo ren False
+  cellLayoutSetAttributes combo ren list (\gen -> [ cellText := name gen ])
+
+  on ok buttonActivated $ do
+    gen <- listStoreGetValue list =<< comboBoxGetActive combo
+    g <- newStdGen
+    putStrLn $ generate gen g
+
+  if tables==[] 
+  then do
+    set edit  [ widgetSensitive := False ]
+    set ok    [ widgetSensitive := False ]
+    set combo [ widgetSensitive := False ]
+  else comboBoxSetActive combo 0
--- homunculus.hs
+++ homunculus.hs
@@ -4,6 +4,7 @@ import Homunculus.Generator
 import Homunculus.Parser
 
 import Graphics.UI.Gtk hiding (Table)
+import System.Directory (getAppUserDataDirectory)
 import System.Environment (getArgs)
 import System.Random
 
@@ -12,7 +13,7 @@ main = main' =<< getArgs
 
 main' :: [String] -> IO ()
 main' args = case args of
-  []            -> putStrLn "Eventually, this will make the UI start up."
+  []            -> start
   ("-h":_)      -> putStrLn helpMessage
   ("--help":_)  -> putStrLn helpMessage
   ("-g":[])     -> putStrLn "No generator file given!"
@@ -35,3 +36,51 @@ helpMessage = concat ["Usage: homunculus [OPTION]... [FILE]...\n\n"
                      ,"                   (Good way to roll dice)\n"
                      ,"  -t             Create a generator for testing purposes\n"
                      ]
+
+start :: IO ()
+start = do
+  initGUI
+
+  {-
+    INITIALIZATION
+  -}
+  window <- windowNew
+
+  menu <- menuBarNew
+
+  file <- menuItemNewWithLabel "File"
+  fileMenu <- menuNew
+  quit <- menuItemNewWithLabel "Quit"
+
+  vbox <- vBoxNew False 0
+  genBox <- vBoxNew False 0
+
+  {-
+    CONSTRUCTION
+  -}
+
+  --I plan on adding more later, so I'm going to go ahead and use mapM_
+  mapM_ (menuShellAppend menu) [file]
+
+  set file    [ menuItemSubmenu := fileMenu ]
+  set vbox    [ containerChild := menu, boxChildPacking menu := PackNatural
+              , containerChild := genBox
+              ]
+  set window  [ containerChild := vbox ]
+
+  {-
+    LOGIC
+  -}
+  --This is the default path where all of your data will be kept. I'm calling this here and
+  --passing it along to functions (rather than calling it as needed) so that, in future
+  --versions, I can have some logic here for user-defined directories.
+  dataPath <- getAppUserDataDirectory "homunculus"
+
+  makeGenerator dataPath genBox
+
+  on quit menuItemActivate mainQuit
+
+  on window objectDestroy mainQuit
+  widgetShowAll window
+
+  mainGUI
